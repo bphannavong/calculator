7cc86ab45ffcb75a87e99e36c8ab5c703b105dde
Billy Phannavong <billy.phannavong@gmail.com>
2022-05-28 19:15:36 -0700

    Implement clear() and sorted button types

--- calculator.js
+++ calculator.js
@@ -23,17 +23,15 @@ let currentValue = 0;
 const display = document.querySelector('#display');
 display.textContent = currentValue;
 
-const buttons = document.querySelectorAll('.button');
-buttons.forEach(button => button.addEventListener('click', calculator));
-//make calculator object? 
+const operatorBtn = document.querySelectorAll('.operator');
+const numberBtn = document.querySelectorAll('.number');
+const evalBtn = document.querySelector('.eval');
+const clearBtn = document.querySelector('.clear');
 
-let CalculatorObj = {
-    name: 'calc',
-}
-for (let i = 0; i < 10; i++){
-    CalculatorObj[i] = i;
-}
-
+operatorBtn.forEach(button => button.addEventListener('click', operation));
+numberBtn.forEach(button => button.addEventListener('click', appendNum));
+clearBtn.addEventListener('click', clear);
+evalBtn.addEventListener('click', operate);
 //    operators
 //buttons change currentValue
 //after operator pressed
@@ -42,24 +40,34 @@ for (let i = 0; i < 10; i++){
 //  then change totalValue --> this.operate(this.operation, totalValue, currentValue)
 //  
 //smth smth arrays, reduce with objects
-function calculator (e) {
+/// ** if number is presseed after operator, then show new totalValue as currentValue, / implement this at beginning of number?
+function operation (e) {
+    const operator = e.target.textContent;
+}
+
+function appendNum(e) {
     const buttonValue = e.target.textContent;
     console.log(buttonValue);
     if (currentValue) {
-        currentValue = (currentValue*10) + CalculatorObj[buttonValue];
+        currentValue = (currentValue*10) + +buttonValue;
     } else {
         currentValue = buttonValue;
     }
     console.log(currentValue);
     display.textContent = currentValue;
 }
-
 function clear() {
     if (!currentValue) {
         totalValue = 0;
+        display.textContent = totalValue;
     } else {
         currentValue = 0;
+        display.textContent = currentValue;
     }
+}
+
+function handleKey(e) {
+
 }
 // link buttons
 // add for each button event listener click    their operator
